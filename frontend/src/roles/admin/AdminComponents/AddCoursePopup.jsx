80a4821ea6acf4c1875aaadf4433bae5de4e75d0
Randira nirman <randiranirman20011210@gmail.com>
2025-03-19 13:27:30 +0530

    fixed the course table

--- frontend/src/roles/admin/AdminComponents/AddCoursePopup.jsx
+++ frontend/src/roles/admin/AdminComponents/AddCoursePopup.jsx
@@ -56,13 +56,13 @@ const AddCoursePopup = ({
             <button
               type="button"
               onClick={onClose}
-              className="px-4 py-2 bg-red text-white font-medium rounded-md hover cursor-pointer hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-colors"
+              className="px-4 py-2 bg-red  text-white font-medium rounded-md hover cursor-pointer hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 transition-colors"
             >
               Cancel
             </button>
             <button
               type="submit"
-              className="px-4 py-2 bg-primary text-white font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
+              className="px-4 py-2 bg-primary text-white cursor-pointer font-medium rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors"
             >
               Save
             </button>
--- frontend/src/roles/admin/AdminComponents/CurrentUser.jsx
+++ frontend/src/roles/admin/AdminComponents/CurrentUser.jsx
@@ -6,9 +6,10 @@ const CurrentUser = () => {
     <>
      <div className="flex flex-col max-w-[80%] border-black mt-1.5 mx-2 ">
          <h1 className="font-semibold text-2xl">Current Users</h1>
-         <thead className="bg-primary text-white font-semibold rounded-lg justify-between">
+         <thead className="bg-primary text-white font-semibold rounded-lg justify-between  ">
             <tr>
-                <th className="p-3 text-left" > Name</th>
+                <th className="p-3 text-left gap-2" > Name</th>
+                <th className="p-3 text-left" > Username</th>
                 <th  className="p-3 text-left" >Email</th>
                 <th className="p-3 text-left" >Role</th>
                 <th className="p-3 text-left" >Action</th>
