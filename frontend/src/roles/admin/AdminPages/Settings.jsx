9ad0112162f154f629715ea642a77bc6355480b0
Randira nirman <randiranirman20011210@gmail.com>
2025-03-30 11:26:27 +0530

    fixed the edit profile

--- frontend/src/roles/admin/AdminPages/Settings.jsx
+++ frontend/src/roles/admin/AdminPages/Settings.jsx
@@ -1,8 +1,27 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Settings = () => {
+   const navigate = useNavigate();
   return (
+    
 
-    <h1>this is  the settings page</h1>
+    <>
+        <h1 className='font-semibold text-2xl'>Settings</h1>
+        <div className=' flex flex-row mt-4'>
+            <div className='max-w-1/2 flex flex-col py-2 space-y-2 px-2 border-1 '>
+
+                <p> FirstName</p>
+                <p> LastName</p>
+                <p> Address</p>
+                <p> ContactNumber</p>
+            <button onClick={() => navigate("/editProfile")} className='bg-primary text-white font-semibold px-1 py-1 rounded-lg items-center cursor-pointer'>Edit Profile </button>
+
+            </div>
+
+        </div>
+
+    </>
   )
 }
 
