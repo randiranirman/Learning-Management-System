481712ec49918541727966d89617921db2215995
randiranirman <randiranirman20011210@gmail.com>
2025-07-18 12:59:00 +0530

    fixed the  subject management

---
+++ frontend/src/roles/admin/AdminPages/ManageClasses.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ManageClasses = () => {
+  return (
+    
+
+    <>
+        <h1>Manage Classes</h1>
+        <p>Here you can create and manage classes, assign students and teachers, and view class details.</p>
+        <p>Use the navigation links to access different sections of class management.</p>
+    </>
+  )
+}
+
+export default ManageClasses
